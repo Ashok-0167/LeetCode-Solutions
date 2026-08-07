f0f19017d16b69f1e214137aadcf83aaa9e73ab4
ASHOK KUMAR P <141118571+Ashok-0167@users.noreply.github.com>
2026-08-07 17:12:48 +0530

    Time: 2 ms (75.84%), Space: 43.5 MB (85.24%) - LeetHub

--- 0833-find-and-replace-in-string/0833-find-and-replace-in-string.java
+++ 0833-find-and-replace-in-string/0833-find-and-replace-in-string.java
@@ -2,7 +2,6 @@ class Solution {
        public String findReplaceString(String S, int[] indexes, String[] sources, String[] targets) {
         Map<Integer, Integer> table = new HashMap<>();
         for (int i=0; i<indexes.length; i++) {
-            // if a match is found in the original string, record it
             if (S.startsWith(sources[i], indexes[i])) {
                 table.put(indexes[i], i);
             }
